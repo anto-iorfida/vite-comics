8a384a077316097306b83f697d0e6355cff3bea3
anto-iorfida <antonioiorfida@hotmail.com>
2024-04-11 14:50:19 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script >
+
+  import AppHeader from './components/AppHeader.vue';
+  export default {
+    components: {
+      AppHeader,
+    }
+  }
+  
+</script>
+
+<template>
+<AppHeader></AppHeader>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+	name: 'AppHeader',
+        data(){
+            return{
+
+            }
+        } 
+}
+</script>
+
+<template>
+    <header>
+        
+    </header>
+</template>
